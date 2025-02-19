6efaeae571fb26a1636160ee1d4c0cbfce28be8f
Lenisha Ghimire <lenishaghimire07@gmail.com>
2025-02-19 09:03:56 +0545

    Navbar and sidebar

---
+++ Frontend/src/components/UserSidebar.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import userlogo from "../assets/userlogo.png";
+import plus from "../assets/plus.png";
+import feed from "../assets/feed.png";
+import help from "../assets/help.png";
+import room from "../assets/room.png";
+import wellness from "../assets/wellness.png";
+import setting from "../assets/setting.png";
+import logout from "../assets/logout.png";
+import notification from "../assets/notification.png";
+
+const UserSidebar = () => {
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="w-64 bg-[#EC993D] text-white flex flex-col p-5">
+        {/* Logo */}
+        <div className="mb-10">
+        <img src={userlogo} alt="Logo" className="w-[300px] h-[70px] ml-[-14px] mt-[-14px]" />
+        </div>
+
+        {/* Menu Items */}
+        <nav className="flex flex-col space-y-6">
+          <div className="flex items-center space-x-3 cursor-pointer hover:opacity-80">
+            <img src={plus} alt="Write Post" className="w-5 h-5" />
+            <span>Write Post</span>
+          </div>
+          <div className="flex items-center space-x-3 cursor-pointer hover:opacity-80">
+            <img src={feed} alt="Feed" className="w-5 h-5" />
+            <span>Feed</span>
+          </div>
+          <div className="flex items-center space-x-3 cursor-pointer hover:opacity-80">
+            <img src={room} alt="Rooms" className="w-5 h-5" />
+            <span>Rooms</span>
+          </div>
+          <div className="flex items-center space-x-3 cursor-pointer hover:opacity-80">
+            <img src={wellness} alt="Wellness Center" className="w-5 h-5" />
+            <span>Wellness Center</span>
+          </div>
+          <div className="flex items-center space-x-3 cursor-pointer hover:opacity-80">
+            <img src={help} alt="I Need Help" className="w-5 h-5" />
+            <span>I Need Help</span>
+          </div>
+        </nav>
+
+        {/* Bottom Settings and Logout */}
+        <div className="mt-auto flex flex-col space-y-6">
+          <div className="flex items-center space-x-3 cursor-pointer hover:opacity-80">
+            <img src={setting} alt="Settings" className="w-5 h-5" />
+            <span>Settings</span>
+          </div>
+          <div className="flex items-center space-x-3 cursor-pointer hover:opacity-80">
+            <img src={logout} alt="Logout" className="w-5 h-5" />
+            <span>Logout</span>
+          </div>
+        </div>
+      </div>
+
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col">
+        {/* Hero Section (Navbar) */}
+        <div className="flex justify-end items-center p-4 bg-white">
+          {/* Notification Icon */}
+          <div className="cursor-pointer mx-4">
+            <img src={notification} alt="Notification" className="w-6 h-6" />
+          </div>
+
+          {/* Profile Placeholder */}
+          <div className="cursor-pointer w-8 h-8 bg-gray-300 rounded-full"></div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default UserSidebar;
